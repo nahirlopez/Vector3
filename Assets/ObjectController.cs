f83bebd538c6971d5dc4050920d289be247bbe7b
nahirlopez <101569242+nahirlopez@users.noreply.github.com>
2022-05-20 09:04:30 -0300

    uwu

---
+++ Assets/ObjectController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectController : MonoBehaviour
+{
+    public float xspeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.x < 7)
+        {
+            transform.position += new Vector3(xspeed,0,0);
+        }
+       
+    }
+}
