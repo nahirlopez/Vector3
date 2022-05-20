8239dc137c9f17bdf2a2f64dfb207ecaa9915cd6
nahirlopez <101569242+nahirlopez@users.noreply.github.com>
2022-05-20 08:51:52 -0300

    Initial commit

---
+++ Assets/ObjectMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectMovement : MonoBehaviour
+{
+    public GameObject MyCube;
+    public Vector3 RotationVector;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MyCube.transform.eulerAngles += RotationVector;
+    }
+}
---
+++ Assets/OjectTransform.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OjectTransform : MonoBehaviour
+{
+    public GameObject MyCube;
+    public Vector3 newScale;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        MyCube.transform.localScale = newScale;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
